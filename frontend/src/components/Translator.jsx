45eee60e1b53cdad1763693083f06d96bfafc728
Priyanka Kannan <priyankannan91@gmail.com>
2024-07-12 15:33:14 -0500

    code blocked

--- frontend/src/components/Translator.jsx
+++ frontend/src/components/Translator.jsx
@@ -4,10 +4,30 @@ import "../App.css";
 function Translator() {
     const [input, setInput] = useState('');
     const [responseMessage, setResponseMessage] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
 
     const handleInputChange = (event) => {
         setInput(event.target.value);
     };
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        // send a POST request to API endpoint
+        // indicate method, headers, and JSON body
+
+        // reset error and response before making a new request
+
+        // assign apiURL: deployed vs local URLs and add a console.log to debug
+
+        // if response is successful (200)
+            // save response as a JSON
+            // update response message to message attached to response data
+        // esle
+            // save response as a JSON
+            // set the error useState to error message attached to response data
+
+    }
 
     return (
         pass

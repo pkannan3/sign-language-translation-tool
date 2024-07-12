528a4bf0bfe99b40aaedad81d04125f512f8a9ad
Priyanka Kannan <priyankannan91@gmail.com>
2024-07-12 15:07:56 -0500

    Added hooks and handleChange

--- frontend/src/components/Translator.jsx
+++ frontend/src/components/Translator.jsx
@@ -2,7 +2,12 @@ import React, { useState } from 'react';
 import "../App.css";
 
 function Translator() {
+    const [input, setInput] = useState('');
+    const [responseMessage, setResponseMessage] = useState('');
 
+    const handleInputChange = (event) => {
+        setInput(event.target.value);
+    };
 
     return (
         pass

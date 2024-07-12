6a149f2627b128c21c0d2a7af7d5a3685ae4748e
Priyanka Kannan <priyankannan91@gmail.com>
2024-07-12 12:09:17 -0500

    Updated settings.py INSTALLED_APPS

--- backend/sign_language_translation/settings.py
+++ backend/sign_language_translation/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "translator.apps.TranslatorConfig",
+    "rest_framework",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [

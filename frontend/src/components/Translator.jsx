3a46b1266234e0d3380523ef16d9fd8245409679
Priyanka Kannan <priyankannan91@gmail.com>
2024-07-12 17:52:06 -0500

    env

--- frontend/src/components/Translator.jsx
+++ frontend/src/components/Translator.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import "../App.css";
+// import "../App.css";
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
